f1c4d7541fb826df0d93987caa22b85f862a704c
Breno Gonçalves <breno999a@gmail.com>
2024-09-30 18:19:23 -0300

    Iniciar projeto

---
+++ main.js
@@ -0,0 +1,53 @@
+const button = document.querySelector(".button-add-task");
+const input = document.querySelector(".input-task ");
+const listaCompleta = document.querySelector(".list-tasks");
+
+let minhaListaDeItens = [];
+
+function adicionarNovaTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  });
+
+  input.value = "";
+
+  mostrarTarefas();
+}
+
+function mostrarTarefas() {
+  let novaLi = "";
+
+  //['comprar café', 'estudar programação']
+
+  minhaListaDeItens.forEach((item, index) => {
+    novaLi =
+      novaLi +
+      `
+
+     <li class="task ${item.concluida && "done"}">
+        <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${index})">
+        <p>${item.tarefa}</p>
+         <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${index})">
+    </li>
+        
+        `;
+  });
+  listaCompleta.innerHTML = novaLi;
+
+  localStorage.setItem("lista", JSON.stringify(minhaListaDeItens));
+}
+
+function concluirTarefa(index) {
+  minhaListaDeItens[index].concluida = !minhaListaDeItens[index].concluida;
+
+  mostrarTarefas();
+}
+
+function deletarItem(index) {
+  minhaListaDeItens.splice(index, 1);
+
+  mostrarTarefas();
+}
+
+button.addEventListener("click", adicionarNovaTarefa);
